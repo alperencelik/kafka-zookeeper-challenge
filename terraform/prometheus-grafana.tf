a49dd4a249499723eb54336126225b36f030f88b
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-01-28 00:10:42 +0300

    add prometheus&grafana.tf

---
+++ terraform/prometheus-grafana.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05788af9005ef9a93"
+  instance_type = "t3.medium"
