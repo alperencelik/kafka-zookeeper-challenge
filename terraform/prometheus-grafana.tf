ccebd047271fe5915bfb9e36eec68b821d1838e9
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-01-28 00:56:55 +0300

    edit variables for module

--- terraform/prometheus-grafana.tf
+++ terraform/prometheus-grafana.tf
@@ -14,7 +14,17 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-05788af9005ef9a93"
-  instance_type = "t3.medium"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  associate_public_ip_address = true
+
+  name = "prometheus-grafana"
+
+  ami                         = "ami-05788af9005ef9a93"
+  instance_type               = "t3.medium"
+  key_name                    = "alp-eren"
+  monitoring                  = false
+
 }
