24cc61fe6c182d8493962c46e96b5bf3c5bc2813
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-01-28 15:07:57 +0300

    Update prometheus-grafana.tf

--- terraform/prometheus-grafana.tf
+++ terraform/prometheus-grafana.tf
@@ -17,13 +17,18 @@ provider "aws" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
+}
   
-  name = "prometheus-grafana"
+resource "aws_instance" "prometheus-grafana" {
+
   ami                         = "ami-05788af9005ef9a93"
   instance_type               = "t3.medium"
   key_name                    = "alp-eren"
   monitoring                  = false
   associate_public_ip_address = true
   vpc_security_group_ids = ["sg-0e6eb7789ff4a10b4"]
-
-}
+    
+    
+    
+    
+  }
