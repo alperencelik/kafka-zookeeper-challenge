db6b66151bd665b437b86ecd25ec5bd7e04a5d56
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-01-28 15:08:55 +0300

    replace module to resource

--- terraform/prometheus-grafana.tf
+++ terraform/prometheus-grafana.tf
@@ -14,11 +14,6 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-}
-  
 resource "aws_instance" "prometheus-grafana" {
 
   ami                         = "ami-05788af9005ef9a93"
