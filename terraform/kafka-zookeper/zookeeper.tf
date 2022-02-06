a47423d929c80f664226d6075f5fb81f456b481e
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-07 00:39:08 +0300

    Update zookeeper.tf

--- terraform/kafka-zookeper/zookeeper.tf
+++ terraform/kafka-zookeper/zookeeper.tf
@@ -1,9 +1,9 @@
-resource "aws_network_interface" "main" {
+#resource "aws_network_interface" "main" {
 #  count           = "${var.count}"
 #  subnet_id       = "${element(var.subnet_ids, count.index)}"
 #  private_ips     = ["${cidrhost(element(var.subnet_cidr, count.index), 1)}"]
 #  security_groups = ["${aws_security_group.zookeeper-server.id}"]
-}
+#}
 
 resource "aws_instance" "zookeeper" {
   count                   = "${var.size_of_cluster}"
@@ -16,7 +16,7 @@ resource "aws_instance" "zookeeper" {
 
 
   tags {
-    Name = "${var.name}-zookeeper-${count.index}"
+    Name = "${var.name}-zookeeper-${size_of_cluster.index}"
   }
 }
 
