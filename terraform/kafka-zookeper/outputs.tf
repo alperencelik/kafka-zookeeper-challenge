270fe1aa255cd8478fc0edbbce7bdcf907f21670
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-07 01:29:12 +0300

    Update outputs.tf

--- terraform/kafka-zookeper/outputs.tf
+++ terraform/kafka-zookeper/outputs.tf
@@ -2,6 +2,6 @@ output "active-kafka" {
   value = aws_instance.active-kafka[0].public_ip
 }
 
-output "active-kafka" {
+output "active-kafka-1" {
   value = aws_instance.active-kafka[1].public_ip
 }
