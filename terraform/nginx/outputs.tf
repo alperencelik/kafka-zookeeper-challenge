0da968848207275d53aa26c7d53e912fd86856dd
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-06 22:00:50 +0300

    Update outputs.tf

--- terraform/nginx/outputs.tf
+++ terraform/nginx/outputs.tf
@@ -1,9 +1,3 @@
-output "ec2_complete_primary_network_interface_id" {
-  description = "The ID of the instance's primary network interface"
-  value       = module.ec2_complete.primary_network_interface_id
-}
-  
-output "ec2_complete_public_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = module.ec2_complete.public_ip
+output "ec2_global_ips" {
+  value = ["${aws_instance.main.*.public_ip}"]
 }
