b2d1674cc01465a0ed48f60aef8e2571d74a086e
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-06 21:52:47 +0300

    add ssh_key_name

--- terraform/nginx/variables.tf
+++ terraform/nginx/variables.tf
@@ -1,4 +1,11 @@
 variable "ami_image_id" {
+  description = "Centos 7 ami id"
   type = string
   default = "ami-05788af9005ef9a93"
+}
+
+variable "ssh_key_name" {
+  description = "The name of an EC2 key-pair"
+  type        = string
+  default     = "alp-eren"
 }
