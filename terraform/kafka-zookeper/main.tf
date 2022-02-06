83c1bc97eeb6811506ee3f52150c364385ea82a5
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-07 01:38:06 +0300

    Update main.tf

--- terraform/kafka-zookeper/main.tf
+++ terraform/kafka-zookeper/main.tf
@@ -42,7 +42,34 @@ resource "aws_instance" "standby-kafka" {
   tags = {
     Name ="standby-kafka-${count.index}"
   }  
+  }
+
+resource "aws_instance" "active-zookeeper" {
+
+  count                     = 2
+  ami                         = var.ami_image_id
+  instance_type               = "t3.small"
+  key_name                    = var.ssh_key_name
+  monitoring                  = false
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["sg-01b121550a99155d7"]
   
- 
+  tags = {
+    Name ="standby-kafka-${count.index}"
+  }  
+  }
+
+resource "aws_instance" "standby-zookeeper" {
+
+  count                     = 2
+  ami                         = var.ami_image_id
+  instance_type               = "t3.small"
+  key_name                    = var.ssh_key_name
+  monitoring                  = false
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["sg-01b121550a99155d7"]
   
+  tags = {
+    Name ="standby-kafka-${count.index}"
+  }  
   }
