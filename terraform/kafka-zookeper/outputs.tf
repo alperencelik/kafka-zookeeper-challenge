9deb4dbb835067bea1334bf427722e40ff745139
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-07 01:32:01 +0300

    Update outputs.tf

--- terraform/kafka-zookeper/outputs.tf
+++ terraform/kafka-zookeper/outputs.tf
@@ -1,7 +1,23 @@
-output "active-kafka" {
-  value = aws_instance.active-kafka[0].public_ip
+output "active-kafka-0" {
+  value = aws_instance.active-kafka[0].private_ip
 }
 
 output "active-kafka-1" {
-  value = aws_instance.active-kafka[1].public_ip
+  value = aws_instance.active-kafka[1].private_ip
+}
+
+output "active-kafka-2" {
+  value = aws_instance.active-kafka[2].private_ip
+}
+
+output "standby-kafka-0" {
+  value = aws_instance.standby-kafka[0].private_ip
+}
+
+output "standby-kafka-1" {
+  value = aws_instance.standby-kafka[1].private_ip
+}
+
+output "standby-kafka-2" {
+  value = aws_instance.standby-kafka[2].private_ip
 }
