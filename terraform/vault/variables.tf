169d7044a949ba4d81bf55f9289a5270be648fc6
alperencelik <80721144+alperencelik@users.noreply.github.com>
2022-02-07 03:33:13 +0300

    Update variables.tf

--- terraform/vault/variables.tf
+++ terraform/vault/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
     type = string
     description = "The AWS region you want your region-specific resources provisioned in."
-    default = "eu-north-1"
+    default = "us-west-2"
 }
 
 variable "azs" {
     type = list(string)
     description = "A valid list of Availability Zones (AZs) to deploy your regional resources into."
-    default = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+    default = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
 
 variable "resource_name_prefix" {
     type = string
     description = "all of your resources will have their identifiers prefixed with this string, so you can tell them apart."
-    default = "vault-cluster"
+    default = "tutorialinux"
 }
 
- variable "private_subnet_tags" {
+variable "private_subnet_tags" {
     type = map(string)
     description = "Explicitly passing in the subnet tags that we'll deploy Vault into, so that both the VPC prereq module and the vault-ent module have the same data and don't depend on each other."
-    default = { "Vault": "subnet-289c3041" }
+    default = { "Vault": "deploy" }
 }
